dcde62527c90fe16baa71186a483addd5901f46e
simrandhiman <simrandhiman3121@gmail.com>
2025-01-17 23:27:08 +0530

    UI_Login_and_Sign

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,19 @@ import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import cookieParser from "cookie-parser"
+import cors from "cors";
 
 dotenv.config();
 const PORT=process.env.PORT ;
 const app=express();
 app.use(express.json());
 app.use(cookieParser())  //get values from it while change photo 
+
+app.use(cors({ 
+    origin: 'http://localhost:5173',
+     credentials: true
+}));
+
  
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>
+      I'm  Profilepage 
+    </div>
+  )
+}
+
+export default ProfilePage
